e8a8e0fb29556acaaa282642b1cc4ebb752ea3f1
Gotkwondo <dhwngus99@gmail.com>
2022-01-20 22:36:02 +0900

    Change for my web

--- MOMENTUM/JS/background.js
+++ MOMENTUM/JS/background.js
@@ -4,6 +4,6 @@ const chosenImg = images[Math.floor(Math.random() * images.length)];
 const bgimg = document.createElement("img");
 bgimg.src = `img/${chosenImg}`;
 
-document.body.appendChild(bgimg);
+document.body.querySelector("#bg_img").appendChild(bgimg);
 //document.body.appendChild()는 body 끝에 태그를 추가할 때 필요
 //document.body.prependChild()는 body 맨 앞에 태그를 추가할 때 필요
