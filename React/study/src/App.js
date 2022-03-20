1668885904c4166e59691ba5ccbbc8c85db20525
Gotkwondo <dhwngus99@gmail.com>
2022-03-20 22:33:43 +0900

    React_study_day_2

---
+++ React/study/src/App.js
@@ -0,0 +1,25 @@
+// import { Fragment } from 'react';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    // <Fragment>
+    //    sdfsd
+    // </Fragment >
+
+    //<>
+    //  <h1>1</h1>
+    //  <h1>2</h1>
+    //</>
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
