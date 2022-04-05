0af83818978f423d7aaf3d58bcd46340344185e2
Gotkwondo <dhwngus99@gmail.com>
2022-04-05 22:42:06 +0900

    ComponentRepeat

--- React/study/src/index.js
+++ React/study/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App/App4';
+import App from './App/App6';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
