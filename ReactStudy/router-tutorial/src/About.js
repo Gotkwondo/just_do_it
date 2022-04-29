569935cbe83a1df022702fe463d6938304db1535
Gotkwondo <dhwngus99@gmail.com>
2022-04-29 21:11:09 +0900

    react-router_URLQuery

--- ReactStudy/router-tutorial/src/About.js
+++ ReactStudy/router-tutorial/src/About.js
@@ -1,8 +1,15 @@
+import { useLocation } from 'react-router-dom';
+
 const About = () => {
+  const { search } = useLocation();
+
+  //현재 지금 경로가(search) "?detail=true"인지 확인
+  const showDetail = search === "?detail=true";
   return (
     <div>
       <h1>소개</h1>
       <p>이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 프로젝트입니다.</p>
+      {showDetail && <p>detail 값을 true로 설정되있음</p>}
     </div>
   );
 };
