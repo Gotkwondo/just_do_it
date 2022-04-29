42729c8e24358a54367519f6318b36aff8f089e1
Gotkwondo <dhwngus99@gmail.com>
2022-04-29 19:09:01 +0900

    react-route_MultiPath

--- ReactStudy/router-tutorial/src/App.js
+++ ReactStudy/router-tutorial/src/App.js
@@ -16,8 +16,12 @@ const App = () => {
       <hr />
       {/* 새로운 버전의 react-route에서는 Route는 Routes를 사용해 래핑해야하고 element={<Home />}의 형태로 보여줄 컴포넌트를 설정해주면 된다 */}
       <Routes>
-        <Route path="/" element={<Home />} exact={true} />
-        <Route path="/about" element={<About/>} />
+        {/* v6 부터는 exact 옵션이 삭제됬다. */}
+        <Route path="/" element={<Home />} />
+
+        {/* v6부터 path는 문자열만 올 수 있기에 배열 형태로 여러개를 적용하려면 map함수를 이용해야 한다. */}
+        {/* <Route path="/about" element={<About/>} /> */}
+        {["/about", "/info"].map(path => <Route key={path} path={path} element={<About/>}/>)}
       </Routes>
     </div>
   );
