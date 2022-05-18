f84ce45f8687fb0517899edd18a040b0366801de
Gotkwondo <dhwngus99@gmail.com>
2022-05-18 20:16:51 +0900

    redux-middleware_refactoring

--- ReactStudy/learn-redux-middleware/src/container/SampleContainer.js
+++ ReactStudy/learn-redux-middleware/src/container/SampleContainer.js
@@ -13,8 +13,17 @@ const SampleContainer = ({
 }) => {
   //  클래스 형태 컴포넌트였다면 componentDidMount
   useEffect(() => {
-    getPost(1);
-    getUsers(1);
+    // useEffect 에 파라미터로 넣는 함수는 async 로 할 수 없기 때문에
+    // async 함수를 선언하고 호출함
+    const fn = async () => {
+      try {
+        await getPost(1);
+        await getUsers(1);
+      } catch (e) {
+        console.log(e); // 에러 조회
+      }
+    };
+    fn();
   }, [getPost, getUsers]);
   return (
     <Sample
@@ -28,11 +37,11 @@ const SampleContainer = ({
 
 //  컴포넌트와 리덕스를 연동
 export default connect(
-  ({ sample }) => ({
+  ({ sample, loading }) => ({
     post: sample.post,
     users: sample.users,
-    loadingPost: sample.loadingPost,
-    loadingUsers: sample.loadingUsers
+    loadingPost: loading[`sample/GET_POST`],
+    loadingUsers: loading[`sample/GET_USERS`]
   }),
   {
     getPost,
