31a7967fcf733169f1e5dec78918f76099e49056
Gotkwondo <dhwngus99@gmail.com>
2022-05-19 20:52:13 +0900

    redux-middlware_redux-saga_비동기카운터

--- ReactStudy/learn-redux-middleware/src/index.js
+++ ReactStudy/learn-redux-middleware/src/index.js
@@ -4,17 +4,21 @@ import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 // import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const logger = createLogger();
+const sagaMiddleware = createSagaMiddleware();  //  비동기 카운터 만들기
 const store = configureStore({
   reducer: rootReducer,
-  middleware: [logger, ReduxThunk],
+  middleware: [logger, ReduxThunk, sagaMiddleware], //  비동기 카운터 만들기
 });
+sagaMiddleware.run(rootSaga);
 
 root.render(
   <Provider store={store}>
--- ReactStudy/learn-redux-middleware/src/modules/index.js
+++ ReactStudy/learn-redux-middleware/src/modules/index.js
@@ -1,13 +1,21 @@
 //  루트 리듀서
 import { combineReducers } from 'redux';
 import counter from './counter';
+import { counterSaga } from './counterWithSaga';
 import sample from './sample';
 import loading from './loading';
+import { all } from '@redux-saga/core/effects';
 
 const rootReducer = combineReducers({
   counter,
   sample,
   loading
-})
+});
+
+//  비동기 카운터 만들기
+export function* rootSaga() {
+  //  all함수는 여러 사가를 합쳐주는 역활을 한다.
+  yield all([counterSaga()]);
+}
 
 export default rootReducer;
