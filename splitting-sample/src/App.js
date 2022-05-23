e242040881065fd8919895ddda69a093a79954bc
Gotkwondo <dhwngus99@gmail.com>
2022-05-23 17:32:35 +0900

    codeSplitting_React_splittingWithReactLazy&Suspense

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,29 +1,24 @@
-import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, Suspense } from 'react';  // 책에서는 React를 import 하지 않았지만 실제 해보니 import해주지 않으면 동작을 안한다.
+const SplitMe = React.lazy(() => import('./SplitMe'));
 
-class App extends Component{
-  state = {
-    SplitMe: null
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
   };
-  handleClick = async () => {
-    const loadedModule = await import('./SplitMe');
-    this.setState({
-      SplitMe: loadedModule.default
-    });
-  };
-  render() {
-    const { SplitMe } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handleClick}>Hello React</p>
-          {SplitMe && <SplitMe />}
-        </header>
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick}>Hello React</p>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible&&<SplitMe />}
+        </Suspense>
+      </header>
+    </div>
+  )
 }
 
 export default App;
