b0e8b518212685a7912f99b495bdd89854c8ea8c
Gotkwondo <dhwngus99@gmail.com>
2022-05-23 18:19:42 +0900

    codeSplitting_React_splittingWithLoadableComponent

--- ReactStudy/splitting-sample/src/App.js
+++ ReactStudy/splitting-sample/src/App.js
@@ -1,21 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, Suspense } from 'react';  // 책에서는 React를 import 하지 않았지만 실제 해보니 import해주지 않으면 동작을 안한다.
-const SplitMe = React.lazy(() => import('./SplitMe'));
+import { useState } from 'react';
+import loadable from '@loadable/component'
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>
+});
 
 const App = () => {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React</p>
-        <Suspense fallback={<div>loading...</div>}>
-          {visible&&<SplitMe />}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>Hello React</p>
+        {visible&&<SplitMe />}
       </header>
     </div>
   )
