a27d57c69fbaa9c9b8a68699f4987051119dceab
Gotkwondo <dhwngus99@gmail.com>
2022-05-23 18:32:07 +0900

    codeSplitting_React_splittingWithLoadableComponent(2)

--- ReactStudy/splitting-sample/src/App.js
+++ ReactStudy/splitting-sample/src/App.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import loadable from '@loadable/component'
 const SplitMe = loadable(() => import('./SplitMe'), {
   fallback: <div>loading...</div>
-});
+}); //  loadable 함수의 두번째 파라미터에는 속성을 넣을 수 있다. https://loadable-components.com/docs/api-loadable-component/
 
 const App = () => {
   const [visible, setVisible] = useState(false);
@@ -13,7 +13,8 @@ const App = () => {
   };
   const onMouseOver = () => {
     SplitMe.preload();
-  };
+  };  //  커서가 올라가면 preload()로 미리 로딩되게 함
+      //  preload()외에 다른 기능들이 있음 (공식문서 확인 https://loadable-components.com/docs/getting-started/)
   return (
     <div className="App">
       <header className="App-header">
