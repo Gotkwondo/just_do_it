c6989764acbbbb8cdc99638ba93c82d7e1313f27
Gotkwondo <dhwngus99@gmail.com>
2022-05-25 15:00:40 +0900

    SSR_writting_serverCode

--- ReactStudy/ssr-recipe/src/index.server.js
+++ ReactStudy/ssr-recipe/src/index.server.js
@@ -1,9 +1,26 @@
-import ReactDOMServer from 'react-dom/server'
+import ReactDOMServer from 'react-dom/server';
+import express from 'express';
+import { StaticRouter } from 'react-router-dom/server';
+import App from './App';
 
-//서버에서 리액트 컴포넌트를 렌더링할 때는 ReactDOMServer의 renderToString함수를 사용한다.
-//이 함수에 JSX를 넣어 호출하면 렌더링 결과를 문자열로 반환한다.
-const html = ReactDOMServer.renderToString(
-  <div>Hello Server Side Rendering!</div>
-);
+const app = express();
 
-console.log(html);
+//  서버 사이드 렌더링을 처리할 핸들러 함수이다.
+const serverRender = (req, res, next) => {
+  //  이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해줌
+  const context = {};
+  const jsx = (
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+  const root = ReactDOMServer.renderToString(jsx);  //  레더링을 함
+  res.send(root); //  클라이언트에게 결과물 응답
+};
+
+app.use(serverRender);
+
+//  5000포트로 서버를 가동
+app.listen(5000, () => {
+  console.log('Running on http://localhost:5000');
+});
