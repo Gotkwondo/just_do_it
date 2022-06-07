f6585751ffe5e7ec01be9836cb00bc75f05fc62e
Gotkwondo <dhwngus99@gmail.com>
2022-06-07 16:02:51 +0900

    SSR_Suspense

--- ReactStudy/splitting-sample/src/App.js
+++ ReactStudy/splitting-sample/src/App.js
@@ -1,29 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import loadable from '@loadable/component'
-const SplitMe = loadable(() => import('./SplitMe'), {
-  fallback: <div>loading...</div>
-}); //  loadable 함수의 두번째 파라미터에는 속성을 넣을 수 있다. https://loadable-components.com/docs/api-loadable-component/
+import React, { useState, Suspense } from 'react';  // 책에서는 React를 import 하지 않았지만 실제 해보니 import해주지 않으면 동작을 안한다.
+const SplitMe = React.lazy(() => import('./SplitMe'));
 
 const App = () => {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
-  const onMouseOver = () => {
-    SplitMe.preload();
-  };  //  커서가 올라가면 preload()로 미리 로딩되게 함
-      //  preload()외에 다른 기능들이 있음 (공식문서 확인 https://loadable-components.com/docs/getting-started/)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick} onMouseOver={onMouseOver}>Hello React</p>
-        {visible&&<SplitMe />}
+        <p onClick={onClick}>Hello React</p>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible&&<SplitMe />}
+        </Suspense>
       </header>
     </div>
   )
 }
 
 export default App;
+// 출처: https://58cjdcns99.tistory.com/entry/React-19장-코드-스플리팅-Reactlazy-Suspense-loadablecomponent [Just 두 It:티스토리]
