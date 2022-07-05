09ae728ae74fb82f33ea273c6ba59d336816b4b0
Gotkwondo <dhwngus99@gmail.com>
2022-07-05 16:49:58 +0900

    How_to_use_Koa_Server_installing

--- ReactStudy/backendProgrammingWithKoa/blog-backend/src/index.js
+++ ReactStudy/backendProgrammingWithKoa/blog-backend/src/index.js
@@ -1,1 +1,13 @@
-const hello = "hello";
+const Koa = require('koa');
+
+const app = new Koa();
+
+//  서버에 접속하면 반환하는 신텍스 작성
+app.use(ctx => {
+  ctx.body = 'hello world';
+});
+
+//  서버 포트 설정
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
