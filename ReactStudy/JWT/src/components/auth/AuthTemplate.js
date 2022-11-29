38d625d38e7e92e42e4117f97c872187e17af133
Gotkwondo <dhwngus99@gmail.com>
2022-11-29 20:00:44 +0900

    AuthTemplate

--- ReactStudy/JWT/src/components/auth/AuthTemplate.js
+++ ReactStudy/JWT/src/components/auth/AuthTemplate.js
@@ -1,11 +1,18 @@
 //  스타일은 styles/component/auth 에 있는 scss 파일을 이용
+import '../../styles/component/auth/authTemplate.scss'
+import { Link } from 'react-router-dom';
 
 // 회원가입/로그인 페이지의 레이아웃을 담당하는 컴포넌트
 
 const AuthTemplate = ({children}) => {
   return (
-    <div className='authTemplateBolck'>
-      {children}
+    <div className='authTemplateBlock'>
+      <div className='whiteBox'>
+        <div className='logo_area'>
+          <Link to='/'>REATERS</Link>
+        </div>
+        {children}
+      </div>
     </div>
   )
 }
