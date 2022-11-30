d208af528118e597d5a736b55cb42d39f6c4e450
Gotkwondo <dhwngus99@gmail.com>
2022-11-30 21:27:32 +0900

    redux-saga를 이용한 loading 리덕스 모듈&createRequestSaga 유틸 함수

--- ReactStudy/JWT/src/modules/index.js
+++ ReactStudy/JWT/src/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth.js';
+import loading from './loading.js';
 
 const rootReducer = combineReducers({
   auth,
+  loading
 });
 
 export default rootReducer;
