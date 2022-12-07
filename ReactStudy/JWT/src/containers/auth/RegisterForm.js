8a92e9bad15e8a7652c4d935a433a48bd28338fa
Gotkwondo <dhwngus99@gmail.com>
2022-12-07 21:13:49 +0900

    RegisterForm 오류 안내 기능

--- ReactStudy/JWT/src/containers/auth/RegisterForm.js
+++ ReactStudy/JWT/src/containers/auth/RegisterForm.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import AuthForm from '../../components/auth/AuthForm';
 import { changeField, initializeForm, register } from '../../modules/auth';
@@ -7,6 +7,7 @@ import { check } from '../../modules/user';
 
 const RegisterForm = () => {
   const navigate = useNavigate();
+  const [error, setError] = useState(null);
 
   const dispatch = useDispatch();
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
@@ -32,8 +33,14 @@ const RegisterForm = () => {
   const onSubmit = e => {
     e.preventDefault();
     const { username, password, passwordConfirm } = form;
+    if ([username, password, passwordConfirm].includes('')) {
+      setError('빈 칸을 모두 입력해주세요');
+      return;
+    }
     if (password !== passwordConfirm) {
-      //  TODO: 오류처리
+      setError('비밀 번호가 일치하지 않습니다.');
+      dispatch(changeField({ form: 'register', key: 'password', value: '' }));
+      dispatch(changeField({ form: 'register', key: 'passwordConfirm', value: '' }));
       return;
     }
     dispatch(register({username, password}))
@@ -47,9 +54,15 @@ const RegisterForm = () => {
   //  회원가입 성공/실패 처리
   useEffect(() => {
     if (authError) {
-      console.log('오류 발생');
-      console.log(authError);
-      return
+      //  이미 계저이 있는 경우
+      // console.log(typeof authError.response.status)
+      if (authError.response.status === 409) {
+        setError('이미 존재하는 계정입니다.');
+        return;
+      }
+      //  기타 이유
+      setError('회원가입 실패');
+      return;
     }
     if (auth) {
       console.log('회원가입 성공');
@@ -70,6 +83,7 @@ const RegisterForm = () => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
